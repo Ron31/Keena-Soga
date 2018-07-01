f4d0c1646bb8a1b7d0eaa1af6c50fb3fba1c32af
Newtox <39810250+Newtox@users.noreply.github.com>
2018-07-01 19:22:42 +0200

    Update toxbot.js

--- toxbot.js
+++ toxbot.js
@@ -29,7 +29,7 @@ bot.on("ready", async () => {
 
     //Goodbye Message
     bot.on("guildMemberRemove", async member => { 
-        if(!message.guild.id!=`406946551538253828`) 
+        if(!=message.guild.id!=`406946551538253828`) 
         bot.channels.get("439880541043425290").send(`${member} hat die ${member.guild.name} verlassen...`)
     });
     bot.on("message", async message => { }) 
@@ -37,7 +37,7 @@ bot.on("ready", async () => {
 
     //Welcome Message
     bot.on("guildMemberAdd", async member => { 
-        if(!message.guild.id!=`406946551538253828`) 
+        if(!=message.guild.id!=`406946551538253828`) 
         bot.channels.get("439880541043425290").send(`${member} Willkommen in der ${member.guild.name}! Bitte wirf einen Blick auf die <#406946551538253830> und benimm dich. \nWenn du fragen hast kannst du gerne auf die Mods, Supporter oder auch auf Newtox zugehen. \nEs schadet auch nicht einen blick in das <#427916685413187604> zu werfen ${bot.emojis.find("name","Glumanda_Hi")}`)
             member.addRole("406952857917456395")
     });
