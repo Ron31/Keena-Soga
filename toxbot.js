338d21b0b7f1ba9cf93f62c03d2e600ebfd986b8
Newtox <39810250+Newtox@users.noreply.github.com>
2018-07-04 17:32:54 +0200

    Update toxbot.js

--- toxbot.js
+++ toxbot.js
@@ -253,7 +253,6 @@ bot.on("ready", async () => {
                     message.member.addRole(message.guild.roles.find("name", Rolle).id).catch(err => {
                         if(err) message.channel.send(`Hm. Da ist was schiefgelaufen. ${message.author}.\n\nError-Bericht: ${err}`)
                     });
-                    message.channel.send(`Du hast jetzt die Rolle ${Rolle}. ${message.author}`)
                 } else {
                     message.channel.send(`Diese Rolle existiert auf dem Server nicht. ${message.author}`)
                 }
