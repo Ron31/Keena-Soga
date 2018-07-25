8430bad4e6dbad634eac2b15311c345e4ca72a91
Newtox <39810250+Newtox@users.noreply.github.com>
2018-07-26 00:54:37 +0200

    Update toxbot.js

--- toxbot.js
+++ toxbot.js
@@ -81,19 +81,24 @@ bot.on("ready", async () => {
 
 
   
-   //Rollenfarbe
-        if(command == "rolecolor") {
-            if(message.author.id == BotSettings.OwnerID || message.member.hasPermission("ADMINISTRATOR")) { 
-            var Rolle = args.join (" ")
-            if(message.guild.roles.find("name", Rolle)) {
-                message.channel.send(`Die Rolle **${Rolle}** hat die RGB-Farbe **${message.guild.roles.find("name", Rolle).hexColor.toUpperCase()}**. ${message.author}`)
-            } else {
-                message.channel.send(`Bitte gebe eine Rolle an. ${message.author}`)
-           
-        } else {
-                message.channel.send(`Das ist keine Rolle auf dem Server. ${message.author}`)
-        } 
-    }        
+   ///Rollenfarbe
+ if(command == "rolecolor") {
+    if(message.author.id == BotSettings.OwnerID || message.member.hasPermission("ADMINISTRATOR")) { 
+    var Rolle = args.join (" ")
+    if(message.guild.roles.find("name", Rolle)) {
+        message.channel.send(`Die Rolle **${Rolle}** hat die RGB-Farbe **${message.guild.roles.find("name", Rolle).hexColor.toUpperCase()}**. ${message.author}`)
+    } else {
+        message.channel.send(`Bitte gebe eine Rolle an. ${message.author}`)
+   
+    }
+
+} else {
+    message.channel.send(`Das ist keine Rolle auf dem Server. ${message.author}`)
+}
+
+ } else {
+     message.channel.send(`Nur der Bot-Owner oder eine Person mit Admin Rechten kann diesen Command nutzen. ${message.author}`)
+ }     
   
            
            
