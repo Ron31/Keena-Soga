f8bec9cce57c4bab4a563a14fef07ca05aef4d08
Newtox <39810250+Newtox@users.noreply.github.com>
2018-08-02 21:09:35 +0200

    Update toxbot.js

--- toxbot.js
+++ toxbot.js
@@ -120,8 +120,8 @@ bot.on("ready", async () => {
         .addField(`ID`,`${mention.id}`,true)
         .addField(`Name`, `${mention.user.username}`)
 
-        if(mention.member.username != mention.member.displayName) {
-            embed.addField(`Nickname`, `${mention.member.displayName}`)
+        if(mention.username != mention.displayName) {
+            embed.addField(`Nickname`, `${mention.displayName}`)
 
         }
 
