b4431adc61be4e706eb9b672d04bfb2f861d1528
Newtox <39810250+Newtox@users.noreply.github.com>
2018-08-09 23:17:12 +0200

    Update toxbot.js

--- toxbot.js
+++ toxbot.js
@@ -285,15 +285,12 @@ bot.on("ready", async () => {
             .setTitle(`Userinfo von ${mention.user.username}#${mention.user.discriminator}`)
             .addField(`Name`, `${mention.user.username}`)
     
-            if(mention.user.username != mention.displayName) {
+            if(mention.displayName) {
                 embed.addField(`Nickname`, `${mention.displayName}`)
     
-            }
+            } else {
+                  embed.addField(`Nickname`,`-`)
     
-            if(mention.user.username == mention.displayName) {
-                embed.addField(`Nickname`, `-`)
-    
-            }
     
             embed.addField(`Status`,`${status_ger[mention.user.presence.status]}`)
             
