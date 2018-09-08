5102a5ef4502fab6bd22f38b53c03d89e53cd388
Justin <39810250+Newtox@users.noreply.github.com>
2018-09-08 21:26:23 +0200

    Update toxbot.js

--- toxbot.js
+++ toxbot.js
@@ -410,7 +410,7 @@ bot.on("message", async message => {
             let totalSeconds = (bot.uptime / 1000);
             let days = Math.floor(totalSeconds / 86400);
             let hours = Math.round(totalSeconds / 3600);
-            let minutes = Math.floor(totalSeconds / 60);
+            let minutes = Math.round(totalSeconds / 60);
             let seconds = Math.floor(totalSeconds - (60*(Math.floor(totalSeconds/60))))
 
 
