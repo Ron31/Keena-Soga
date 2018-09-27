535953e9b412bd2744d3238d04693a22e62b0e18
Justin <39810250+Newtox@users.noreply.github.com>
2018-09-27 16:40:25 +0200

    Update Keena.js

--- Keena.js
+++ Keena.js
@@ -332,7 +332,6 @@ bot.on("message", async message => {
             let totalSeconds = (bot.uptime / 1000);
             let days = Math.floor(totalSeconds / 86400);
             let hours = Math.floor(totalSeconds / 3600);
-            totalSeconds %= 3600;
             let minutes = Math.floor(totalSeconds / 60);
             let seconds = Math.floor(totalSeconds - (60*(Math.floor(totalSeconds/60))))
 
