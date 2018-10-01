fe37aa81bdf0265859c29bd6498450a00e77334d
Justin <39810250+Newtox@users.noreply.github.com>
2018-10-01 14:20:27 +0200

    Update Yuki.js

--- Yuki.js
+++ Yuki.js
@@ -204,10 +204,10 @@ bot.on("message", async message => {
         //Botinfo
         if(message.content == `${BotSettings.prefix}botinfo`) {
 
-            let totalSeconds = (bot.uptime / 1000);
+            let totalSeconds = (bot.uptime / 3600);
             let days = Math.floor(totalSeconds / 86400);
             let hours = Math.floor(totalSeconds / 3600);
-            totalSeconds %= 3600;
+
             let minutes = Math.floor(totalSeconds / 60);
             let seconds = Math.floor(totalSeconds - (60*(Math.floor(totalSeconds/60))))
 
@@ -639,10 +639,10 @@ bot.on("message", async message => {
         //Uptime-BOT
         if(message.content.startsWith(`${BotSettings.prefix}uptime`)) {
 
-            let totalSeconds = (bot.uptime / 1000);
+            let totalSeconds = (bot.uptime / 3600);
             let days = Math.floor(totalSeconds / 86400);
             let hours = Math.floor(totalSeconds / 3600);
-            totalSeconds %= 3600;
+
             let minutes = Math.floor(totalSeconds / 60); 
             let seconds = Math.floor(totalSeconds - (60*(Math.floor(totalSeconds/60))))
 
